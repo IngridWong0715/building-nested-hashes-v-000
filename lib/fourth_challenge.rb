57af9cf6732760a6840659e56c5f68e76b4bdfc6
Ingrid Wong <yw340@georgetown.edu>
2018-01-14 21:21:15 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,17 +22,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attidude => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attidude => "worried"
-        }
-      ]
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+        ]
    }
 }
 
