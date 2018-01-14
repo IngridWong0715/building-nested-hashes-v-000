0e1ca0781a37602dd477993da2053e74cdd002ab
Ingrid Wong <yw340@georgetown.edu>
2018-01-14 21:07:02 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,17 +2,18 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-          :name => "Lord Montague",  
+          :name => "Lord Montague",
           :age => "53"
         },
       :matriarch => {
-          :name => "Lady Montague"
+          :name => "Lady Montague",
+          :age => "54"
         },
       :hero => {
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -27,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
